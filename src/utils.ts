de3a427c19f6e9cf97e3256c0e2a562d87f16860
Eda <58330360+rivea0@users.noreply.github.com>
2024-07-16 21:56:13 +0300

    Fix type error in `isValidURLString`

--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,11 @@
 import DOMPurify from 'isomorphic-dompurify';
 
 export function isValidURLString(str: string) {
-  let givenURL;
   try {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    givenURL = new URL(str);
+    return Boolean(new URL(str)); 
   } catch (e) {
     return false;
   }
-  return true;
 }
 
 export function sanitizeStr(str: string) {
